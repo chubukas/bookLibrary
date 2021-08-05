db8ed470bd95b1d69ebf79696144b7226726f4d5
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2021-08-05 15:04:27 +0100

    feat(intial commit): this is the intial commit of the project

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express"); // include express module to the package
+const { ApolloServer, gql } = require("apollo-server-express"); // include apollo module
+const cors = require("cors");
+require("dotenv").config();
+
+const DbConnection = require("./db");
+
+// create an express instance
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use(cors());
+
+// connect to database
+DbConnection();
+
+// Graphql schema Query
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// create a resolver for the query schema
+const resolvers = {
+  Query: {
+    hello: () => "hello world",
+  },
+};
+
+(async () => {
+  // create an instance of the apollo server
+  const apollosever = new ApolloServer({ typeDefs, resolvers });
+
+  // start the apollo server
+  await apollosever.start();
+
+  // apply the apollo graphql middleware and set the path to /api
+  apollosever.applyMiddleware({ app, path: "/api" });
+})();
+
+app.listen(PORT, () => console.log("listening on port 4000"));
